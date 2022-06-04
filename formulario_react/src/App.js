e5ff06ff09a52b6661ecfb88ae909136d075b947
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-04 01:45:19 -0300

    criando componente form.js e form.css, iniciando o jsx <form><table><input>

---
+++ formulario_react/src/App.js
@@ -0,0 +1,16 @@
+//component
+import Form from "./Components/Form";
+
+//style
+import "./App.css";
+import "../src/PagesStyle/Form.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ formulario_react/src/Components/Form.js
@@ -0,0 +1,15 @@
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <table className="container">
+          <div className="context">
+            <input type="text" placeholder="Nome"></input>
+          </div>
+        </table>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
