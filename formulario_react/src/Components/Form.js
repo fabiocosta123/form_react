5a13f4ffd1956805128348be54741043d8e00ac6
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-04 22:03:07 -0300

    criando função para pegar valores atraves do onChange

--- formulario_react/src/Components/Form.js
+++ formulario_react/src/Components/Form.js
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [telefone, setTelefone] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+    console.log(name);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    console.log(email);
+  };
+
+  const handleTelefone = (e) => {
+    setTelefone(e.target.value);
+    console.log(telefone);
+  };
+
   return (
     <div>
       <form>
@@ -10,18 +31,21 @@ const Form = () => {
                 type="text"
                 name="name"
                 placeholder="Digite seu nome"
+                onChange={handleName}
               ></input>
               <span>E-mail: </span>
               <input
                 type="email"
                 name="email"
                 placeholder="Digite seu e-mail"
+                onChange={handleEmail}
               ></input>
               <span>Telefone</span>
               <input
                 type="number"
                 name="telefone"
                 placeholder="Digite seu telefone"
+                onChange={handleTelefone}
               ></input>
             </label>
             <button type="submit" className="btn">
