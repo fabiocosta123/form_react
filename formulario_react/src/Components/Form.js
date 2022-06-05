43bd37c13303f97c90bcb7b35f789e7de5cda43c
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-04 21:41:27 -0300

    inserindo nome, email, telefone e estilizando body, input, span

--- formulario_react/src/Components/Form.js
+++ formulario_react/src/Components/Form.js
@@ -4,7 +4,29 @@ const Form = () => {
       <form>
         <table className="container">
           <div className="context">
-            <input type="text" placeholder="Nome"></input>
+            <label>
+              <span>Nome: </span>
+              <input
+                type="text"
+                name="name"
+                placeholder="Digite seu nome"
+              ></input>
+              <span>E-mail: </span>
+              <input
+                type="email"
+                name="email"
+                placeholder="Digite seu e-mail"
+              ></input>
+              <span>Telefone</span>
+              <input
+                type="number"
+                name="telefone"
+                placeholder="Digite seu telefone"
+              ></input>
+            </label>
+            <button type="submit" className="btn">
+              Enviar
+            </button>
           </div>
         </table>
       </form>
